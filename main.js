2dbfdbfec7286ae579979928299f7747b86b299a
Rethabile <rethabileliphalane213@gmail.com>
2026-09-19 09:33:26 +0000

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-npx
+const express=require("express")
+const app=express()
+require("dotenv").config
+
+console.log(process.env.DATABASE_URL)
+
+
+const port=process.env.PORT||3000
+
+app.listen(port,()=>{
+    console.log("port is running")
+})
